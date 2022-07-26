38983423a84601476648a925b69c21ec6d050f1b
NatC02 <NatanCeballos02@gmail.com>
2022-07-26 12:07:12 -0400

    ternary operator based on user auth state

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,20 @@
+import { auth, googleAuthProvider } from '../lib/firebase';
+
 export default function EnterPage({ }) {
+  const user = null;
+  const username = null;
+
+  // 1. user signed out displaying <SignInButton />
+  // 2. user signed in, but missing username, then <UsernameForm />
+  // 3. user signed in, has username displaying <SignOutButton />
   return (
     <main>
-        <h1>Sign Up</h1>
+      {/* ternary operator for UI logic based on username state */}
+      {user ? 
+        !username ? <UsernameForm /> : <SignOutButton /> 
+        : 
+        <SignInButton />
+      }
     </main>
-  )
+  );
 }
