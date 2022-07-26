d7da8bcae656a83f84693e384d14f2baa82f60e4
NatC02 <NatanCeballos02@gmail.com>
2022-07-26 10:28:18 -0400

    react toast messages

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import toast from "react-hot-toast"
+
 export default function Home() {
   return (
     <div>
       <h1>Home, Welcome</h1>
+      <button onClick={() => toast.success('Testing some toast msg')}>
+        Toast Test 
+      </button>
     </div>
   )
 }
