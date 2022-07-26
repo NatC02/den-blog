b5ad703afd8c629697ab5944f764cd1a4ae47b8a
NatC02 <NatanCeballos02@gmail.com>
2022-07-26 13:14:06 -0400

    passing auth hook for user state via component react context

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,18 @@ import Navbar from "../components/Navbar";
 import { Toaster } from "react-hot-toast";
 
 import { UserContext } from '../lib/context';
+import { useUserData } from '../lib/hooks';
 
 function MyApp({ Component, pageProps }) {
+
+  const userData = useUserData();
+
   return (
     <>
-      <UserContext.Provider value={{ user: {}, username: 'natan' }}>
-      <Navbar />
-      <Component {...pageProps} />
-      <Toaster />
+      <UserContext.Provider value={userData}>
+        <Navbar />
+        <Component {...pageProps} />
+        <Toaster />
       </UserContext.Provider>
     </>
   );
