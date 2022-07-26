860e92902a122b6ed3d0c620d8f6d8881d1d7079
NatC02 <NatanCeballos02@gmail.com>
2022-07-26 12:10:44 -0400

    Sign in with google funtion

--- pages/enter.js
+++ pages/enter.js
@@ -18,3 +18,20 @@ export default function EnterPage({ }) {
     </main>
   );
 }
+
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    try {
+      await auth.signInWithPopup(googleAuthProvider);
+      } catch (err) {
+      console.log(err);
+      }
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={'/google.png'} /> Sign in with Google
+    </button>
+  );
+}
