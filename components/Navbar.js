f94f5bf8bda91ed77040293ceca399088a46d297
NatC02 <NatanCeballos02@gmail.com>
2022-07-26 09:59:06 -0400

    placeholder user sign-in state

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+// navbar
+export default function Navbar() {
+//   temp state to be updates with auth state from firebase
+  const user = null;
+  const username = null;
+
+  return (
+  );
+}
