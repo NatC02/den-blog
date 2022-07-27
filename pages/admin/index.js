19bc487d5928232eb3fa205931ac7d0ab2ec2dcf
NatC02 <NatanCeballos02@gmail.com>
2022-07-27 19:45:26 -0400

    creating new post component and pass firestore data to component

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -42,7 +42,63 @@ function PostList() {
   );
 }
 
+
+function CreateNewPost() {
+  const router = useRouter();
+  const { username } = useContext(UserContext);
+  const [title, setTitle] = useState('');
+
+  // Ensure slug is URL safe by converting to kebab-case
+  const slug = encodeURI(kebabCase(title));
+
+  // Validate length of title
+  const isValid = title.length > 3 && title.length < 100;
+
+  // Create a new post in firestore
+  const createPost = async (e) => {
+    e.preventDefault();
+    const uid = auth.currentUser.uid;
+    const ref = firestore.collection('users').doc(uid).collection('posts').doc(slug);
+
+    // Tip: give all fields a default value here
+    // helps prevent errors when creating a new post for accessing fields that might not exist
+    const data = {
+      title,
+      slug,
+      uid,
+      username,
+      published: false,
+      content: '# hello world!',
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+      heartCount: 0,
+    };
+
+    // Create a new post in firestore
+    await ref.set(data);
+
+    toast.success(`Post ${data.title} created!`)
+
+    // Imperative navigation after doc is set
+    router.push(`/admin/${slug}`);
+
+  };
+
+
   return (
-    <form>
+    <form onSubmit={createPost}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="My Awesome tech DIY tutorial"
+        className={styles.input}
+      />
+      <p>
+        <strong>Slug Title:</strong> {slug}
+      </p>
+      <button type="submit" disabled={!isValid} className="btn-green">
+        Create New Post
+      </button>
     </form>
   );
+}
