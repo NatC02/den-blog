94413948ca3083fa304c390d68ef490dcbdc91cb
NatC02 <NatanCeballos02@gmail.com>
2022-07-27 08:17:41 -0400

    query document to get user document posts

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -7,6 +7,24 @@ export async function getServerSideProps({ query }) {
 
   const userDoc = await getUserWithUsername(username);
 
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    // make a query from the user document to get all posts
+    const postsQuery = userDoc.ref
+      .collection('posts')
+      .where('published', '==', true)
+      // most recent first
+      .orderBy('createdAt', 'desc')
+      .limit(5);
+      // get the posts and map each document to document data
+      // needs to be serializable
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
   return {
     props: { user, posts }, // these objects will be passed to the pages component (as props)
   };
