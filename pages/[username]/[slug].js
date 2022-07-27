e46c37efa5e7640d856794f3b8473a21e4309385
NatC02 <NatanCeballos02@gmail.com>
2022-07-27 13:02:56 -0400

    ISR for user posts with getStaticProps

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,3 +1,32 @@
+import { getUserWithUsername, postToJSON } from '../../lib/firebase';
+
+// fetch data at build time to pre-render the page (caching) 
+export async function getStaticProps({ params }) {
+  // get user with username from url params
+  const { username, slug } = params;
+  // get user from user document in firestore
+  const userDoc = await getUserWithUsername(username);
+
+  let post;
+  let path;
+
+  if (userDoc) {
+    // Make reference to username with slug as ID
+    const postRef = userDoc.ref.collection('posts').doc(slug);
+    // convert to JSON
+    post = postToJSON(await postRef.get());
+
+    // prop as path to re-fetch on the client-side to rehydrate
+    path = postRef.path;
+  }
+
+  return {
+    props: { post, path },
+    // regenrate the page every (time) seconds
+    revalidate: 5000,
+  };
+}
+
 export default function Post(props) {
 
   return (
