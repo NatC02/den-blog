da560acd3701dab06dea66a0792352d1005f11e3
NatC02 <NatanCeballos02@gmail.com>
2022-07-27 12:57:57 -0400

    refactored user create post

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,8 +1,8 @@
-// acts as the id to fetch the user's post from the firestore database
-export default function PostPage({ }) {
-    return (
-      <main>
-          <h1>Posts</h1>
-      </main>
-    )
-  }
+export default function Post(props) {
+
+  return (
+    <main>
+
+    </main>
+  );
+}
