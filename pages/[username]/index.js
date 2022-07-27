8ee4ebaebd91e7f953bb8d4d4599d5cf49fc8fbb
NatC02 <NatanCeballos02@gmail.com>
2022-07-27 08:04:36 -0400

    async server-side rendering function, retuns user and post

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,6 +1,11 @@
 import UserProfile from '../../components/UserProfile';
 import PostFeed from '../../components/PostFeed';
 
+export async function getServerSideProps({ query }) {
+  return {
+    props: { user, posts }, // these objects will be passed to the pages component (as props)
+  };
+}
 
 
 export default function UserProfilePage({ user, posts }) {
