ea54df530cdc449dc4cbbf3ed23db343ba51be82
NatC02 <NatanCeballos02@gmail.com>
2022-07-27 19:15:39 -0400

    dependencies and PostList component

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,11 +1,48 @@
-import AuthCheck from '../../components/AuthCheck';
+import styles from "../../styles/Admin.module.css";
+import AuthCheck from "../../components/AuthCheck";
+import PostFeed from "../../components/PostFeed";
+import { UserContext } from "../../lib/context";
+import { firestore, auth, serverTimestamp } from "../../lib/firebase";
 
-export default function AdminPostPage({}) {
+import { useContext, useState } from "react";
+import { useRouter } from "next/router";
+
+import { useCollection } from "react-firebase-hooks/firestore";
+import kebabCase from "lodash.kebabcase";
+import toast from "react-hot-toast";
+
+export default function AdminPostsPage(props) {
   return (
     <main>
       <AuthCheck>
-        <h1>Admin Page</h1>
+        <PostList />
+        <CreateNewPost />
       </AuthCheck>
     </main>
   );
 }
+
+function PostList() {
+  // current user doc, then post subcollection
+  const ref = firestore
+    .collection("users")
+    .doc(auth.currentUser.uid)
+    .collection("posts");
+  const query = ref.orderBy("createdAt");
+  // read collection in realtime
+  const [querySnapshot] = useCollection(query);
+  // map document to document data
+  const posts = querySnapshot?.docs.map((doc) => doc.data());
+
+  return (
+    <>
+      <h1>Manage your Posts</h1>
+      <PostFeed posts={posts} admin />
+    </>
+  );
+}
+
+  return (
+    <form>
+    </form>
+  );
