2edd88a20f41440ab944e97c2d31a01ff76f3501
NatC02 <NatanCeballos02@gmail.com>
2022-07-27 14:46:54 -0400

    hydrate server props to real time data (realtime data hydration)

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,9 +1,9 @@
-import styles from '../../styles/Post.module.css';
-import PostContent from '../../components/PostContent';
-import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase';
-import { useDocumentData } from 'react-firebase-hooks/firestore';
+import styles from "../../styles/Post.module.css";
+import PostContent from "../../components/PostContent";
+import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
 
-// fetch data at build time to pre-render the page (caching) 
+// fetch data at build time to pre-render the page (caching)
 export async function getStaticProps({ params }) {
   // get user with username from url params
   const { username, slug } = params;
@@ -15,7 +15,7 @@ export async function getStaticProps({ params }) {
 
   if (userDoc) {
     // Make reference to username with slug as ID
-    const postRef = userDoc.ref.collection('posts').doc(slug);
+    const postRef = userDoc.ref.collection("posts").doc(slug);
     // convert to JSON
     post = postToJSON(await postRef.get());
 
@@ -26,13 +26,13 @@ export async function getStaticProps({ params }) {
   return {
     props: { post, path },
     // regenrate the page every (time) seconds
-    revalidate: 5000,
+    revalidate: 10000,
   };
 }
 
 export async function getStaticPaths() {
   // query all posts in firestore
-  const snapshot = await firestore.collectionGroup('posts').get();
+  const snapshot = await firestore.collectionGroup("posts").get();
 
   // map each post to a path
   const paths = snapshot.docs.map((doc) => {
@@ -48,16 +48,33 @@ export async function getStaticPaths() {
     //   { params: { username, slug }}
     // ],
     paths,
-    // when user reaches a page that doesn't exist, then use next's server-side fallback 
-    fallback: 'blocking',
+    // when user reaches a page that doesn't exist, then use next's server-side fallback
+    fallback: "blocking",
   };
 }
 
 export default function Post(props) {
+  // make ref to database using path from props
+  const postRef = firestore.doc(props.path);
+  // useDocumentData hook to get real-time feed of data (this costs me more money) two reads 
+  const [realtimePost] = useDocumentData(postRef);
+
+  // props post wont care about the source of the data, it will only care about the data
+  const post = realtimePost || props.post;
+
+  // The post value will prefer the realtimePost value if it exists, otherwise it will just use the server rendered props while loading
 
   return (
     <main className={styles.container}>
+      <section>
+        <PostContent post={post} />
+      </section>
 
+      <aside className="card">
+        <p>
+          <strong>{post.heartCount || 0} 🤍</strong>
+        </p>
+      </aside>
     </main>
   );
 }
