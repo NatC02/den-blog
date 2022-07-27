dc06ae49250afc7d5d656dbef68ba16d696c5ae5
NatC02 <NatanCeballos02@gmail.com>
2022-07-27 08:11:35 -0400

    import for retrieving username from url

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,5 +1,6 @@
 import UserProfile from '../../components/UserProfile';
 import PostFeed from '../../components/PostFeed';
+import { getUserWithUsername } from '../../lib/firebase';
 
 export async function getServerSideProps({ query }) {
   const { username } = query;
