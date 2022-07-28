76ced0434f4e711f002d690290bd790322829247
NatC02 <NatanCeballos02@gmail.com>
2022-07-28 08:59:43 -0400

    dependencies, AuthCheck component, and PostManager custom component

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,7 +1,21 @@
-export default function AdminPostPage({ }) {
-    return (
-      <main>
-          <h1>Admin Posts</h1>
-      </main>
-    )
-  }
+import styles from '../../styles/Admin.module.css';
+import AuthCheck from '../../components/AuthCheck';
+import { firestore, auth, serverTimestamp } from '../../lib/firebase';
+
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+// treat a form like react-hook state
+import { useForm } from 'react-hook-form';
+import ReactMarkdown from 'react-markdown';
+import Link from 'next/link';
+import toast from 'react-hot-toast';
+
+export default function AdminPostEdit(props) {
+  return (
+    <AuthCheck>
+        <PostManager />
+    </AuthCheck>
+  );
+}
