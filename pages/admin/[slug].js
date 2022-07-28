0a4e5cc8fc214e2f28055dd5ad6836dba4c854f7
NatC02 <NatanCeballos02@gmail.com>
2022-07-28 09:10:14 -0400

    Post Manager function to handle react state with firestore hooks

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -5,7 +5,7 @@ import { firestore, auth, serverTimestamp } from '../../lib/firebase';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 
-import { useDocumentData } from 'react-firebase-hooks/firestore';
+import { useDocumentData, useDocumentDataOnce } from 'react-firebase-hooks/firestore';
 // treat a form like react-hook state
 import { useForm } from 'react-hook-form';
 import ReactMarkdown from 'react-markdown';
@@ -17,5 +17,34 @@ export default function AdminPostEdit(props) {
     <AuthCheck>
         <PostManager />
     </AuthCheck>
+  );
+}
+
+function PostManager() {
+  // toggle for previewing markdown
+  const [preview, setPreview] = useState(false);
+
+  const router = useRouter();
+  const { slug } = router.query; // slug is the id of the post
+
+  const postRef = firestore.collection('users').doc(auth.currentUser.uid).collection('posts').doc(slug);
+  // listen to post data only when component is mounted
+  const [post] = useDocumentDataOnce(postRef);
+
+  return (
+    <main className={styles.container}>
+      {post && (
+        // wnen post object is available, show the form
+        <>
+          <section>
+            <h1>{post.title}</h1>
+            <p>ID: {post.slug}</p>
+
+            {/* Custom post form states to send to firestore */}
+            <PostForm postRef={postRef} defaultValues={post} preview={preview} />
+          </section>
+        </>
+      )}
+    </main>
   );
 }
